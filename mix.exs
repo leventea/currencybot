772110a1762981f96dabd1b3a465de2364d39ee0
Levente <leventea@tuta.io>
2023-05-28 20:54:24 +0200

    Configure Quantum, use Tzdata

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule CurrencyBot.MixProject do
       {:nadia, "~> 0.7"}, # telegram lib
       {:jason, "~> 1.4"}, # used for parsing http responses
       {:quantum, "~> 3.5"}, # used for periodically refreshing currency rates
+      {:tzdata, "~> 1.1"}, # Used by quantum for timezones
       {:httpoison, "~> 1.7"} # used for fetching currency rates, locked to 1.7 by nadia
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
