960f340e645479b8afa1d79f174fa4e813161c50
Levente <leventea@tuta.io>
2023-05-28 22:17:52 +0200

    Make convert function less error-prone

--- lib/currency/exchange.ex
+++ lib/currency/exchange.ex
@@ -1,21 +1,24 @@
 defmodule CurrencyBot.Currency.Exchange do
   alias CurrencyBot.Currency
 
-  import Enum, only: [map: 2]
+  import Enum, only: [map: 2, filter: 2]
 
   @spec convert(float(), String.t(), list(String.t())) :: list({String.t(), float()})
   def convert(amount, from, target_currencies) do
     # REVIEW: bottleneck?
     table = Currency.Store.call({:get_table})
 
-    [{^from, source_rate}] = :ets.lookup(table, from)
-
-    target_currencies
-    |> map(fn curr -> :ets.lookup(table, curr) end)
-    |> map(fn [w] -> w end) # unwrap from extra array
-    |> map(fn {curr, val} ->
-      single = val / source_rate # worth of 1 unit of target currency
-      {curr, amount * single}
-    end)
+    case :ets.lookup(table, from) do
+      [] -> {:err, "invalid source currency"}
+      [{^from, source_rate}] ->
+        target_currencies
+        |> map(fn curr -> :ets.lookup(table, curr) end)
+        |> filter(fn x -> x != [] end) # filter out invalid lookup results
+        |> map(fn [w] -> w end) # unwrap from extra list
+        |> map(fn {curr, val} ->
+          single = val / source_rate # worth of 1 unit of target currency
+          {curr, amount * single}
+        end)
+    end
   end
 end
