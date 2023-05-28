691aa63dc4de0af06737daf859be66fef7f0f550
Levente <leventea@tuta.io>
2023-05-28 23:02:26 +0200

    Return ok on successful conversion

--- lib/currency/exchange.ex
+++ lib/currency/exchange.ex
@@ -3,23 +3,23 @@ defmodule CurrencyBot.Currency.Exchange do
 
   import Enum, only: [map: 2, filter: 2]
 
-  @spec convert(float(), String.t(), list(String.t()), integer()) :: list({String.t(), float()})
+  @spec convert(float(), String.t(), list(String.t()), integer()) :: {:ok, list({String.t(), float()})} | {:err, String.t()}
   def convert(amount, from, target_currencies, round_to \\ 2) do
     # REVIEW: bottleneck?
     table = Currency.Store.call({:get_table})
 
     case :ets.lookup(table, from) do
       [] -> {:err, "invalid source currency"}
-      [{^from, source_rate}] ->
-        target_currencies
-        |> map(fn curr -> :ets.lookup(table, curr) end)
-        |> filter(fn x -> x != [] end) # filter out invalid lookup results
-        |> map(fn [w] -> w end) # unwrap from extra list
-        |> map(fn {curr, val} ->
-          single = val / source_rate # worth of 1 unit of target currency
-          rounded = round_to(amount * single, round_to)
-          {curr, rounded}
-        end)
+      [{^from, source_rate}] -> {:ok,
+      target_currencies
+      |> map(fn curr -> :ets.lookup(table, curr) end)
+      |> filter(fn x -> x != [] end) # filter out invalid lookup results
+      |> map(fn [w] -> w end) # unwrap from extra list
+      |> map(fn {curr, val} ->
+        single = val / source_rate # worth of 1 unit of target currency
+        rounded = round_to(amount * single, round_to)
+        {curr, rounded}
+      end)}
     end
   end
 
