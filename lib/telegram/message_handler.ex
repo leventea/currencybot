cb7a0a4d78f022f54f301eb77a488d8615bb6216
Levente <leventea@tuta.io>
2023-05-28 18:59:19 +0200

    extract_text utility

--- lib/telegram/message_handler.ex
+++ lib/telegram/message_handler.ex
@@ -5,7 +5,15 @@ defmodule CurrencyBot.Telegram.MessageHandler do
     end)
   end
 
-  defp handle_message(_event) do
-    :logger.debug("handling event")
+  defp extract_text(event) do
+    case event.message do
+      nil -> nil
+      %{text: text, caption: caption} -> text || caption
+    end
+  end
+
+  defp handle_message(event) do
+    IO.inspect(event)
+    :logger.debug("text: #{extract_text(event)}")
   end
 end
